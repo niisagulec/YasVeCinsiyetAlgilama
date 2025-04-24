a50efe975c4d6908ec7abf0c70546e4f4d45498e
Nisa Güleç <126286088+niisagulec@users.noreply.github.com>
2025-04-24 21:33:50 +0300

    acıklama satırları eklendi

--- yasvecinsiyet.py
+++ yasvecinsiyet.py
@@ -6,11 +6,15 @@ def highlihtFace(net, frame, conf_threshold=0.7):
     frameOpencvDnn = frame.copy()
     frameHeight=frameOpencvDnn.shape[0]
     frameWidth=frameOpencvDnn.shape[1]
+
+    # Görüntüyü blob formatına çeviriyoruz
     blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0,(300, 300), [104, 117, 123], True, False)
 
     net.setInput(blob)
     detections=net.forward()
     faceBoxes=[]
+
+    # Tespit edilen her yüz için konumları hesapla ve dikdörtgen çiz
     for i in range(detections.shape[2]):
         confidence=detections[0,0,i,2]
         if confidence>conf_threshold:
@@ -22,7 +26,7 @@ def highlihtFace(net, frame, conf_threshold=0.7):
             cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
     return frameOpencvDnn,faceBoxes
 
-# Hazır modellerin eklenmesi
+# Hazır modellerin eklendi
 
 faceProto="opencv_face_detector.pbtxt"
 faceModel="opencv_face_detector_uint8.pb"
@@ -31,7 +35,11 @@ ageModel="age_net.caffemodel"
 genderProto="gender_deploy.prototxt"
 genderModel="gender_net.caffemodel"
 
+
+# Modelin ihtiyaç duyduğu ortalama değerler
 MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+
+# Yaş ve cinsiyet sınıfları
 ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList=['Erkek','Kadin']
 
@@ -42,6 +50,8 @@ genderNet=cv2.dnn.readNet(genderModel, genderProto)
 # Resmin tanıtılması algılanması ve tahmin yapılması
 video=cv2.VideoCapture("kızcocuk.jpg" if "kızcocuk.jpg" else 0)
 padding=20
+
+# Sonsuz döngü içinde her kareyi işleyerek yüz, yaş ve cinsiyet tahmini yapılır
 while cv2.waitKey(1)<0 :
     hasFrame, frame=video.read()
     if not hasFrame:
@@ -59,15 +69,22 @@ while cv2.waitKey(1)<0 :
                    :min(faceBox[2]+padding, frame.shape[1]-1)]
         
         blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+        
+        # Cinsiyet tahmini
         genderNet.setInput(blob)
         genderPreds=genderNet.forward()
         gender=genderList[genderPreds[0].argmax()]
         print(f'Cinsiyet: {gender}')
-
+        
+        # Yaş tahmini
         ageNet.setInput(blob)
         agePreds=ageNet.forward()
         age=ageList[agePreds[0].argmax()]
         print(f'Yaş: {age[1:-1]} yasinda')
 
+        # Tahmin sonuçlarını görüntü üzerine yazdırma
         cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
+
+
+        # Sonuçları gösterme
         cv2.imshow("Yas ve Cinsiyet Algilama", resultImg)
